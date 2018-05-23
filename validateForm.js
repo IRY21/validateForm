f6ccc06f2948ae5df9e30dd597d636966c17e1fd
IRY21 <rilok21work@gmail.com>
2018-05-23 11:21:16 +0500

    first commit

---
+++ validateForm.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/**
+ *
+ * @param options
+ formId: 'profile',
+ formValidClass: 'form_valid',
+ formInvalidClass: 'form_invalid',
+ inputErrorClass: 'input_error'
+ */
+function validateForm(options) {
+    var form = document.getElementById(options.formId);
+    var formValidClass = options.formValidClass || 'Form_valid';
+    var formInvalidClass = options.formInvalidClass || 'Form_invalid';
+    var inputErrorClass = options.inputErrorClass || 'Input_error';
+
+    function requiredValid(elem) {
+        if (elem.value == '' && elem.hasAttribute('data-required')) {
+            elem.classList.add(inputErrorClass);
+        }
+    }
+
+    function validElem(elem) {
+        var regExp;
+        var value = elem.value;
+
+        switch (elem.dataset.validator) {
+            case 'empty':
+                if (value == '') elem.classList.add(inputErrorClass);
+
+                requiredValid(elem);
+
+                break;
+            case 'letters':
+                regExp = /^([a-zа-яё]*)$/i;
+                if (!regExp.test(value)) elem.classList.add(inputErrorClass);
+
+                requiredValid(elem);
+
+                break;
+            case 'number':
+                var min, max;
+                if (isNaN(Number(value))) {
+                    elem.classList.add(inputErrorClass);
+                    return;
+                }
+
+                if (elem.dataset.validatorMin) {
+                    min = parseInt(elem.dataset.validatorMin);
+                }
+                if (elem.dataset.validatorMax) {
+                    max = parseInt(elem.dataset.validatorMax);
+                }
+
+                value = parseInt(value);
+
+                if (min > value || value > max) {
+                    elem.classList.add(inputErrorClass);
+                }
+
+                requiredValid(elem);
+
+                break;
+            case 'regexp':
+                regExp = new RegExp(elem.dataset.validatorPattern);
+                if (!regExp.test(value) && value != '') elem.classList.add(inputErrorClass);
+
+                requiredValid(elem);
+
+                break;
+        }
+    }
+
+    function checkForm() {
+        var inputList = form.querySelectorAll('input, select, textarea');
+
+        for (var i = 0; i < inputList.length; i++) {
+            validElem(inputList[i]);
+
+            if (inputList[i].classList.contains(inputErrorClass)) {
+                form.classList.remove(formValidClass);
+                form.classList.add(formInvalidClass);
+                return false;
+            }
+            form.classList.remove(formInvalidClass);
+            form.classList.add(formValidClass);
+        }
+        return true;
+    }
+
+    form.addEventListener('blur', function (event) {
+        validElem(event.target);
+    }, true);
+
+    form.addEventListener('focus', function (event) {
+        if (event.target.classList.contains(inputErrorClass)) {
+            event.target.classList.remove(inputErrorClass);
+        }
+    }, true);
+
+    form.addEventListener('click', function (event) {
+        if (event.target.tagName === 'INPUT' && event.keyCode == 13) {
+            checkForm();
+        }
+    }, true);
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        //checkForm()
+        /**
+         * sendForm();
+         * */
+        if (checkForm()) sendForm(form);
+    });
+}
